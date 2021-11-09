3e6f2a91a53dd27dc68a3343d26d4e86132c93d4
Rohit T P <tprohit9@gmail.com>
2021-11-09 15:11:25 +0530

    added checks for phone number

--- Q4-7/phone.cpp
+++ Q4-7/phone.cpp
@@ -5,9 +5,22 @@
 #include <iostream>
 
 struct Phone {
-    short int area;
-    short int exchange;
-    short int number;
+    short unsigned int area;
+    short unsigned int exchange;
+    short unsigned int number;
+
+    void fix(Phone &phone)
+    {
+        phone.area *= (phone.area < 999) - (phone.area > 999);
+        phone.exchange *= (phone.exchange < 999) - (phone.exchange > 999);
+        phone.number *= (phone.number < 9999) - (phone.number > 9999);
+    }
+
+    bool isValid(Phone &phone)
+    {
+        return phone.area < 999 && phone.exchange < 999 &&  phone.number < 9999 &&
+                phone.area > 100 && phone.exchange > 100 && phone.number > 1000;
+    }
 };
 
 int main() {
@@ -16,6 +29,11 @@ int main() {
 
     std::cout << "Enter your area code, exchange, and number :";
     std::cin >> yourNumber.area >> yourNumber.exchange >> yourNumber.number;
+
+    yourNumber.fix(yourNumber);
+
+    if(!yourNumber.isValid(yourNumber))
+        return std::cout << "Invalid number", 1;
 
     std::cout << "My Number : ";
     std::cout << "(" << myNumber.area << ") " << myNumber.exchange << "-" << myNumber.number << "\n";
